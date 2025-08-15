891b7e4c5c314eac5e0c8d9a8d93fb7c35aed2cf
sps2604 <97956985+sps2604@users.noreply.github.com>
2025-08-15 20:00:43 +0530

    Create Day_1.cpp

---
+++ Day_1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+            else { 
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
+
+int main() {
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+    Solution s;
+    s.sortColors(nums);
+
+    cout << "Sorted colors: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
